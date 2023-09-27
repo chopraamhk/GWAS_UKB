8daaa0a76be8042a465d52de481a09023fe119c1
Mehak Chopra <m.chopra1@universityofgalway.ie>
2023-09-27 15:40:42 +0100

    Update qc.sh

--- qc.sh
+++ qc.sh
@@ -1,1 +1,19 @@
+#!/bin/sh 
+#SBATCH --job-name="QC"
+#SBATCH -o qc.o%j
+#SBATCH -e qc.e%j
+#SBATCH --mail-user=<>
+#SBATCH --mail-type=ALL
+#SBATCH --partition="highmem"
+#SBATCH -n 64
+#SBATCH -N 2
+
+module load plink2
+
+#code 
+
+for i in {1..22};
+do
+plink2 --threads 64 --bfile /data3/mchopra/ukb_genotype_mri_passed/distensibility_c_"$i" --mach-r2-filter 0.2 --maf 0.001 --geno 0.02 --hwe 0.0000005 midp --indep-pairwise 1000 100 0.8 --rm-dup exclude-all list --make-bed --out QC_"$i"
+done
 
