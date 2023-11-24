a1c39943cd33550f6755cfdd9afe731fa71908ec
Mehak Chopra <m.chopra1@universityofgalway.ie>
2023-11-24 18:57:44 +0000

    Update 11. fastgwapgs.sh

--- 11. fastgwapgs.sh
+++ 11. fastgwapgs.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=<>
 #SBATCH --mail-type=ALL
 
-../gcta-1.94.1 --bfile /home/mchopra/data3/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID} --grm-sparse /home/mchopra/data3/ukb_genotype_mri_passed/output/after_pca_wbi/makesparseGRM/sp_grm --fastGWA-mlm --pheno ../../../phenotypes/norm_pheno_aao.txt --qcovar qcovars_aao_${SLURM_ARRAY_TASK_ID}  --covar ../makeCovar/fixed.txt --threads 32 --out ../results_pgs/PGS_adj_lmm.aao.${SLURM_ARRAY_TASK_ID}
+../gcta-1.94.1 --bfile /home/mchopra/data3/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID} --grm-sparse /home/mchopra/data3/ukb_genotype_mri_passed/output/after_pca_wbi/makesparseGRM/sp_grm --fastGWA-mlm --pheno ../../../phenotypes/norm_pheno_aao.txt --qcovar ../makepgsCovars/qcovars_aao_${SLURM_ARRAY_TASK_ID}  --covar ../makeCovar/fixed.txt --threads 32 --out ../results_pgs/PGS_adj_lmm.aao.${SLURM_ARRAY_TASK_ID}
 
-../gcta-1.94.1 --bfile /home/mchopra/data3/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID} --grm-sparse /home/mchopra/data3/ukb_genotype_mri_passed/output/after_pca_wbi/makesparseGRM/sp_grm --fastGWA-mlm --pheno ../../../phenotypes/norm_pheno_dao.txt --qcovar qcovars_dao_${SLURM_ARRAY_TASK_ID}  --covar ../makeCovar/fixed.txt --threads 32 --out ../results_pgs/PGS_adj_lmm.dao.${SLURM_ARRAY_TASK_ID}
+../gcta-1.94.1 --bfile /home/mchopra/data3/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID} --grm-sparse /home/mchopra/data3/ukb_genotype_mri_passed/output/after_pca_wbi/makesparseGRM/sp_grm --fastGWA-mlm --pheno ../../../phenotypes/norm_pheno_dao.txt --qcovar ../makepgsCovars/qcovars_dao_${SLURM_ARRAY_TASK_ID}  --covar ../makeCovar/fixed.txt --threads 32 --out ../results_pgs/PGS_adj_lmm.dao.${SLURM_ARRAY_TASK_ID}
 
