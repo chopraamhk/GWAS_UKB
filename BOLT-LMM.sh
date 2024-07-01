5745c61ac566ee2c595aa96635f47dc615f54241
Mehak Chopra <m.chopra1@universityofgalway.ie>
2024-07-01 23:06:01 +0100

    Update BOLT-LMM.sh

--- BOLT-LMM.sh
+++ BOLT-LMM.sh
@@ -1,23 +1,21 @@
 #!/bin/sh 
-#SBATCH --job-name="bolt_m"
+#SBATCH --job-name="bolt"
 #SBATCH --mail-user=<>
 #SBATCH --mail-type=ALL
-#SBATCH -o bolt_m.o%j
-#SBATCH -e bolt_m.e%j
-#SBATCH --partition="normal","highmem"
+#SBATCH -o bolt.o%j
+#SBATCH -e bolt.e%j
+#SBATCH --partition="highmem"
 #SBATCH -a 1-22
 
-# Run lmm on all chromosomes to generate sum statistics to be used by bolt
-../../../../BOLT-LMM_v2.4.1/bolt --bed=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID}.bed \
-        --bim=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/c_${SLURM_ARRAY_TASK_ID}.bim \
-        --fam=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/c_1.fam \
+# Run lmm on all chromosomes to generate summ statistics to be used by bolt
+../../../../BOLT-LMM_v2.4.1/bolt --bfile=../../wbi_removedicd10_pruned/ld_${SLURM_ARRAY_TASK_ID} \
         --phenoFile=/data3/mchopra/ukb_genotype_mri_passed/phenotypes/norm_pheno_aao.txt \
         --phenoCol=AAo_distensibility \
         --geneticMapFile=/data3/mchopra/BOLT-LMM_v2.4.1/tables/genetic_map_hg19_withX.txt.gz \
         --LDscoresFile=/data3/mchopra/BOLT-LMM_v2.4.1/tables/LDSCORE.1000G_EUR.tab.gz \
-        --numThreads 32 \
-        --covarMaxLevels=200 \
-        --statsFile=results/ao_GRM_with_imputed \
+        --numThreads=${SLURM_ARRAY_TASK_ID} \
+        --covarMaxLevels=100 \
+        --statsFile=results/ao_GRM_bfile_${SLURM_ARRAY_TASK_ID} \
         --qCovarCol=PC{1:10} \
         --qCovarCol=CENTRE \
         --qCovarCol=BATCH \
@@ -29,15 +27,14 @@
         --qCovarCol=DBP \
         --qCovarCol=SBP \
         --covarFile=bolt_covars.txt \
-        --bgenFile=/data4/UKB/Genotypes/Imputed/bgen/chr${SLURM_ARRAY_TASK_ID}.bgen \
-        --remove=in.plink_but_not_bgen.sample \
-        --noBgenIDcheck \
-        --statsFileBgenSnps=results/ao_Stats.imputed.bgen \
-        --bgenMinMAF=1e-4 \
-        --bgenMinINFO=0.4 \
-        --sampleFile=/data4/UKB/Genotypes/Imputed/bgen/chr1.sample \
+        --sampleFile=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/bgen/chr1.sample \
+        --bgenFile=/data3/mchopra/ukb_genotype_mri_passed/output/wbi_removedICD10/bgen/chr${SLURM_ARRAY_TASK_ID}.bgen \
+        --statsFileBgenSnps=results/ao_bgen_${SLURM_ARRAY_TASK_ID}.stats \
+        --bgenMinMAF=1e-3 \
+        --bgenMinINFO=0.3 \
         --LDscoresMatchBp \
-        --lmmForceNonInf \
+        --lmm \
         --verboseStats \
+        --numLeaveOutChunks 2 \
         --covarUseMissingIndic \
         --maxModelSnps 4000000
